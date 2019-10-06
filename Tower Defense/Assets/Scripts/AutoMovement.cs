d48ee1cdd73fde150369b9f6c0182913dc7424cc
chuiizeet <chuy.guerra19@outlook.es>
2019-10-06 03:42:25 -0500

    Move enemies

---
+++ Tower Defense/Assets/Scripts/AutoMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class AutoMovement : MonoBehaviour
+{
+    [SerializeField] private NavigationNode startingNode;
+    [SerializeField] private float speed = 1.0f;
+
+    private NavigationNode targetNode;
+
+    void Start()
+    { 
+        targetNode = startingNode;
+    }
+
+    void Update()
+    {
+        if (targetNode != null)
+        {
+            var currentPosition = transform.position;
+            var targetPosition = targetNode.gameObject.transform.position;
+            transform.position = Vector2.MoveTowards(currentPosition, targetPosition, speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (targetNode == null) { return; }
+        if (other.gameObject == targetNode.gameObject)
+        {
+            targetNode = targetNode.GetNextNode();
+        }
+    }
+
+}
