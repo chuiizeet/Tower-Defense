70fed737b722c76aa9958a96989bd2047789cf6a
chuiizeet <chuy.guerra19@outlook.es>
2019-10-07 00:32:54 -0500

    Use events, add waves

---
+++ Tower Defense/Assets/Scripts/PausableTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausableTime : MonoBehaviour
+{
+    public float Timer {get; private set;} 
+
+    private void Start()
+    {
+        ResetTimer();
+    }
+
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+    }
+
+    protected void ResetTimer()
+    {
+        Timer = 0.0f;
+    }
+}
---
+++ Tower Defense/Assets/Scripts/Waves/Wave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class Wave : PausableTime
+{
+    protected int currentInstructionIndex;
+
+    public event EventHandler<EventArgTemplate<bool>> WaveCompleted;
+
+    public virtual void BeginWave()
+    {
+        CompleteNextInstruction();
+    }
+
+    protected virtual void HandleCompletion(bool success)
+    {
+        if (WaveCompleted != null) {
+            var completion = new EventArgTemplate<bool>(success);
+            WaveCompleted.Invoke(this, completion);
+        }
+    }
+
+    protected abstract void CompleteNextInstruction();
+
+}
